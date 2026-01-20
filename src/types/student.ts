2d40782242a93155097f0b3b35db1b356419018e
pragunysss <pragunya.wadhwa@edu.sait.ca>
2026-01-20 16:18:45 -0700

    made changes

--- src/types/student.ts
+++ src/types/student.ts
@@ -1,8 +1,9 @@
 // TODO: Create a type for student ids, which should be strings
-
+type StudentId = string;
 // TODO: Create a type for student statuses, which can either be "Full-time", or "Part-time"
-
+type StudentStatus = "Full-time" | "Part-time";
 // TODO: Create an interface for students
+export interface Student {
 // The interface should include the following properties:
 // an id (hint: use the StudentId type created above)
 // a name
@@ -12,8 +13,17 @@
 // an optional github username
 // an optional fun fact
 
+id : StudentId;
+name : string; 
+year : number;
+status : StudentStatus;
+interest : string[];
+githubUsername? : string;
+funFact? : string;
+
 // once all group members have completed their sections, add the following fields:
 // an optional program id
 // an optional array of current course ids
-
-export interface Student {}
+programId? : string;
+currentCourseIds? : string[];
+}
